dd2376fc21d724bc84c2fa381023f0403760694f
Inkception <marvin.eichler12348@gmail.com>
2022-11-18 23:07:10 +0100

    Add MatchQuery.MatchQueryBuilder#tie to report ties

--- core/src/main/java/at/stefangeyer/challonge/model/query/MatchQuery.java
+++ core/src/main/java/at/stefangeyer/challonge/model/query/MatchQuery.java
@@ -6,8 +6,22 @@ import lombok.Data;
 @Data
 @Builder
 public class MatchQuery {
-    private Long winnerId;
+    /**
+     * winnerId can be either a long identifying the winner of the match or the string "tie" to declare the match as tied.
+     **/
+    private Object winnerId;
     private Integer votesForPlayer1;
     private Integer votesForPlayer2;
     private String scoresCsv;
+
+    public static class MatchQueryBuilder {
+        public MatchQueryBuilder winnerId(final Long winnerId) {
+            this.winnerId = winnerId;
+            return this;
+        }
+        public MatchQueryBuilder tie() {
+            this.winnerId = "tie";
+            return this;
+        }
+    }
 }
